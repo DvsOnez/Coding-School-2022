3c4c569772a9f2e38793cbf5cd642ea09595d221
Dionisios Dimitropoulos <mental_dot@hotmail.com>
2022-03-02 14:10:48 +0200

    Added Session-02b

---
+++ Session-02b/Program.cs
@@ -0,0 +1,14 @@
+﻿// 1 ) 
+Console.WriteLine("Hello Dionisis");
+//2 ) 
+int x = 2;
+int y = 3;
+int sum = x + y;
+double div = (double)x/y;
+
+Console.WriteLine("the sum is :" + sum );
+Console.WriteLine("the div is :" + div);
+
+//3 ) 
+
+Console.WriteLine();
